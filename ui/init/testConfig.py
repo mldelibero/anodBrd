09b6d3947f823af910f432c6e46c0185cc6062e0
Michael DeLibero <mldelibero@gmail.com>
2012-02-01 21:48:10 -0500

    changed vars to a single dictionary - testParam

--- ui/init/testConfig.py
+++ ui/init/testConfig.py
@@ -8,39 +8,33 @@ from xlwt import Workbook
 from xlutils import *
 from tempfile import TemporaryFile
 from collections import namedtuple
+from datetime import *
 
 #Initialize testParam_name
-testParam_name = (['','','','','','','','','',])
-testParam_name[0] = 'User Name'
-testParam_name[1] = 'Anod Material'
-testParam_name[2] = 'Electrolyte'
-testParam_name[3] = 'Ch1 Vol (V)'
-testParam_name[4] = 'Ch1 Cur (mA)'
-testParam_name[5] = 'Ch1 Time (min)'
-testParam_name[6] = 'Ch2 Vol (V)'
-testParam_name[7] = 'Ch2 Cur (mA)'
-testParam_name[8] = 'Ch2 Time (min)'
-#Initialize sample testParam_val
-testParam_val = (['','','','','','','','','',])
-testParam_val[0] = 'Michael DeLibero'
-testParam_val[1] = 'Titanium'
-testParam_val[2] = 'H3PO4'
-testParam_val[3] = 0
-testParam_val[4] = 1
-testParam_val[5] = 2
-testParam_val[6] = 3
-testParam_val[7] = 4
-testParam_val[8] = 5
+date = datetime.now().strftime("%Y-%m-%d")
+
+usrName = 'User Name'
+testDate = 'Test Date'
+anodMat = 'Anod Material'
+elecro = 'Elecrolyte'
+ch1_vol = 'Ch1 Vol(V)'
+ch2_vol = 'Ch2 Vol(V)'
+ch1_cur = 'Ch1 Cur(mA)'
+ch2_cur = 'Ch2 Cur(mA)'
+ch1_time = 'Ch1 Time'
+ch2_time = 'Ch2 Time'
+
+testParam = {usrName:'Michael DeLibero',testDate:date,anodMat:'Titanium',elecro:'H3PO4',ch1_vol:0,ch1_cur:1,ch1_time:2,ch2_vol:3,ch2_cur:4,ch2_time:5}
 
 vol_lim = 30    # V
 cur_lim = 100   # mA
 time_lim = 1440 # min (24hrs)
 
-limits = {testParam_name[3]:vol_lim,testParam_name[4]:cur_lim,testParam_name[5]:time_lim,testParam_name[6]:vol_lim,testParam_name[7]:cur_lim,testParam_name[8]:time_lim}
+limits = {ch1_vol:vol_lim,ch1_cur:cur_lim,ch1_time:time_lim,ch2_vol:vol_lim,ch2_cur:cur_lim,ch2_time:time_lim}
+order = [usrName,testDate,anodMat,elecro,ch1_vol,ch1_cur,ch1_time,ch2_vol,ch2_cur,ch2_time]
 
 #Initialize state variable
-state = {testParam_name[3]:0,testParam_name[4]:1,testParam_name[5]:2,testParam_name[6]:3,testParam_name[7]:4,testParam_name[8]:5,'ch1_state':6,'ch2_state':7}
-
+state = {ch1_vol:0,ch1_cur:1,ch1_time:2,ch2_vol:3,ch2_cur:4,ch2_time:5}
 
 template_file = 'testConfig_template.xls'
 testConfig_file = 'testConfig.xls' # Must be based from template
@@ -50,10 +44,12 @@ def create_template(file):
     book = Workbook()
     sheet1 = book.add_sheet('test_info')
 
-    for i in range(len(testParam_name)):
-        sheet1.write(i,0,testParam_name[i])
-        sheet1.write(i,1,testParam_val[i])
-    
+    row = 0
+    for key in order:
+        sheet1.write(row,0,key)
+        sheet1.write(row,1,testParam[key])
+        row+=1
+
     book.save(file)
     book.save(TemporaryFile())
 
@@ -65,18 +61,18 @@ def matches_template(file):
         open(file)
     except IOError as e:
         print "Error: File %s does not exist!" % file
-        print "Rename '%s' to '%s' and fill in your desired test testParam_val." % (template_file,testConfig_file)
+        print "Rename '%s' to '%s' and fill in your desired test testParam values.\n" % (template_file,testConfig_file)
         return False
 
     #Test to see if the testConfig_file matches the template
     allGood = True
     book = open_workbook(file)
     sheet = book.sheet_by_index(0)
-    names = sheet.col_values(0,0,len(testParam_name));
+    names = sheet.col_values(0,0,len(testParam));
 
-    for i in range(len(testParam_name)):
-        if (names[i] != testParam_name[i]):
-            print "%s   !=  %s" % (names[i],testParam_name[i])
+    for row in range(len(order)):
+        if(names[row] != order[row]):
+            print "%s   !=  %s" % (names[row],order[row])
             allGood = False
           
     if (allGood == True):
@@ -92,35 +88,48 @@ def check_usr_limits(file):
     allGood = True
     book = open_workbook(file)
     sheet = book.sheet_by_index(0)
-    usr_val= sheet.col_values(1,0,len(testParam_name));
-    r_val = sheet.col_values(0,0,len(testParam_name));
-    k = sorted(limits.keys())
+    usr_val = sheet.col_values(1,0,len(order))
+    usr_val_name = sheet.col_values(0,0,len(order))
 
-    for row in range(3,len(testParam_name)):
-        if (usr_val[row] > limits[r_val[row]]):
-            print "%s : %i !< limit of %i" % (r_val[row],usr_val[row],limits[r_val[row]])
+    for row in range(4,len(order)):
+        if (usr_val[row] > limits[usr_val_name[row]]):
+            print "%s : %i !< limit of %i" % (usr_val_name[row],usr_val[row],limits[usr_val_name[row]])
             allGood = False
+
     if (allGood == True):
         print "Parameters are below maximums"
         return allGood
     else:
         return False
 
-def confirm_testParameters():
+def confirm_testParameters(file):
     """ Confirm the test parameters with the user."""
     print "\nConfirming test parameters:"
     print "Are all of the following correct?\n"
+
+    allGood = True
+    book = open_workbook(file)
+    sheet = book.sheet_by_index(0)
+    usr_val = sheet.col_values(1,0,len(order))
+    usr_val_name = sheet.col_values(0,0,len(order))
+
+    for row in range(len(usr_val)):
+        
+        
+
+    print state
     for key in state.keys():
-        print "%s = %i" % (key,state[key])
+       print key 
+       print state[key]
+       # print "%s = %i" % (key,state[key])
     return True
 
 def checkUsr():
     """Check that the test configuration file is properly formatted with sensible parameters."""
 
     create_template(template_file)#OverWr old templates is desired
-    if matches_template(testConfig_file) & check_usr_limits(testConfig_file) & confirm_testParameters():
-        print "Initialization pases"
+    if (matches_template(testConfig_file) and check_usr_limits(testConfig_file) and confirm_testParameters(testConfig_file)):
+        print "Initialization Sucessful\n"
     else:
         print "Error: Initialization Failed!"
         return False
-    

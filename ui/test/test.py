e9815b680884a820ef00f9a99937d474e7057434
Michael DeLibero <mldelibero@gmail.com>
2012-03-02 10:54:26 -0500

    began testing of usr limits on requested settings.

--- ui/test/test.py
+++ ui/test/test.py
@@ -1,23 +1,27 @@
 #! /usr/bin/env python
 
-def create_template_test():
-    from init.progConfig import create_template
-    from init.state import state_entire
-    from init.progConfig import template_file
-    bulkState = state_entire()
-    create_template(template_file,bulkState.init_wrDict())
+from init.state import state_entire
+from init.progConfig import testConfig_file
+from init.progConfig import template_file
 
-def matches_template_test():
-    from init.progConfig import matches_template
-    from init.state import state_entire
-    from init.progConfig import testConfig_file
-    bulkState = state_entire()
-    matches_template(testConfig_file,bulkState.init_wrDict())
-
+bulkState = state_entire()
+    
 def state_test():
     """Test the modules in the state class"""
     print stateEntire.init_rwrKeys()
     print stateEntire.init_wrDict()
+
+def create_template_test():
+    from init.progConfig import create_template
+    create_template(template_file,bulkState.init_wrDict())
+
+def matches_template_test():
+    from init.progConfig import matches_template
+    matches_template(testConfig_file,bulkState.init_wrDict())
+
+def check_usr_limits_test():
+    from init.progConfig import check_usr_limits
+    
 
 if __name__ == "__main__":
     import sys
